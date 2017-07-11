0b0e38c088c506083b597b50d631e896c1bd808f
Dan Jones <dan@danielrayjones.com>
2017-07-10 21:37:07 -0500

    🖪 Added setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git config gitflow.branch.master master
+git config gitflow.branch.develop develop
+
+git config gitflow.prefix.feature feature/
+git config gitflow.prefix.release release/
+git config gitflow.prefix.hotfix hotfix/
+git config gitflow.prefix.support support/
+git config gitflow.prefix.versiontag v
+
+git checkout master
+git checkout develop
