5fa824856f8559b9f4ed6b045b4b3aa23321277a
Dan Jones <djones109+c9@gmail.com>
2018-05-11 17:45:03 +0000

    Generate id on game create, instead of using autoincrement

--- models/game.js
+++ models/game.js
@@ -2,6 +2,9 @@
 
 /* global module */
 
+const idGen = require('../libs/idgen');
+var Promise = require('bluebird');
+
 module.exports = function(sequelize, DataTypes) {
   const Game = sequelize.define('Game', {
     name: {
@@ -10,6 +13,21 @@ module.exports = function(sequelize, DataTypes) {
     }
   }, {
     underscored: true,
+    hooks: {
+      beforeCreate: function (game, options) {
+        if (!game.id) {
+          return (async function() {
+            let id = false;
+            while (!id) {
+              let newId = idGen();
+              let count = await Game.count({where:{id: newId}});
+              if (count === 0) id = newId;
+            }
+            game.id = id;
+          })();
+        }
+      }
+    }
   });
 
   Game.associate = function(models) {
