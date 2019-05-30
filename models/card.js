40e0f8aadd88eefc3f4fff13481bef78c0e4c9d9
Dan Jones <dan@raven.com>
2019-05-30 11:38:29 -0500

    🐛 Fix validation error
    Validation error caused by upgrade to Sequelize 5
    
    Fixes #14

--- models/card.js
+++ models/card.js
@@ -24,6 +24,7 @@ module.exports = function(sequelize, DataTypes) {
   Card.associate = function(models) {
     Card.belongsTo(models.Deck, {
       foreignKey: {
+        name: 'deck_id',
         allowNull: false
       },
       onDelete: 'CASCADE'
